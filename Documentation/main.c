9462916a0c4d015ee6e63ac102f5f6fe2d70a21b
ArmenAri <armenaristakes@gmail.com>
2018-11-21 15:37:37 +0100

    Initial Commit

---
+++ Documentation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Documentation
+//
+//  Created by Armen Aristakessyan on 21/11/2018.
+//  Copyright © 2018 ISIMA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
